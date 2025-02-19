9ab4c288c88e57bb6399a077fef44c4c42d7f78e
larajeangupid <lgupid@paterostechnologicalcollege.edu.ph>
2025-02-19 22:23:53 +0800

    Added name: Lara Jean Gupid

--- activity1.java
+++ activity1.java
@@ -1,19 +1,9 @@
-public class StudentNames {
+public class Activity1 {
     public static void main(String[] args) {
         System.out.println("List of Students:");
-
-<<<<<<< HEAD
-        // System.out.println("Mykah Gabejan - BSIT 2M");
-=======
-        // Students, add your names below using the format:
-        // System.out.println("Your Name - SECTION");
-        // Example:
-        // System.out.println("Juan Dela Cruz - BSIT 2M");
-        System.out.println("Xander Olimberio - BSIT 2M");
-        System.out.println("Stephanie Capuno - BSCS 2M");
-        System.out.println("Clarence P. Cardenas - BSIT 2M");
-        System.out.println("James Michael B. Dela Cruz - BSIT 2M");
-      System.out.println("Mykah Gabejan - BSIT 2M");
-
+   
+        // Collaborators: Add your names below using the format:
+        // System.out.println("Your Full Name");
+       // System.out.println("Lara Jean Gupid");
     }
 }
