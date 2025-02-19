98bcdc33b4ae3374a412a56accb3802d24d2fe64
Sugui05 <mmsugui@paterostechnologicalcollege.edu.ph>
2025-02-19 21:14:25 +0800

    Update activity1.java

--- activity1.java
+++ activity1.java
@@ -2,18 +2,16 @@ public class StudentNames {
     public static void main(String[] args) {
         System.out.println("List of Students:");
 
-<<<<<<< HEAD
-        // System.out.println("Mykah Gabejan - BSIT 2M");
-=======
         // Students, add your names below using the format:
         // System.out.println("Your Name - SECTION");
         // Example:
         // System.out.println("Juan Dela Cruz - BSIT 2M");
+        
         System.out.println("Xander Olimberio - BSIT 2M");
         System.out.println("Stephanie Capuno - BSCS 2M");
         System.out.println("Clarence P. Cardenas - BSIT 2M");
         System.out.println("James Michael B. Dela Cruz - BSIT 2M");
-      System.out.println("Mykah Gabejan - BSIT 2M");
-
+        System.out.println("Mykah Gabejan - BSIT 2M");
+        System.out.println("Ma. Liza M. Sugui - BSIT 2M"); // Your name added here
     }
 }
