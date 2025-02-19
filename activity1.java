ebc93b6fcbe94130adcd52b3856733e5fc1a2c54
johnpeter <jelloren@paterostechnologicalcollege.com>
2025-02-19 21:20:34 +0800

    Added name: John peter Elloren

--- activity1.java
+++ activity1.java
@@ -1,9 +1,9 @@
-public class StudentNames {
+public class Activity1 {
     public static void main(String[] args) {
         System.out.println("List of Students:");
-        // Students, add your names below using the format:
-        // System.out.println("Your Name - SECTION");
-        // Example:
-        // System.out.println("Juan Dela Cruz - BSIT 2M");
+   
+        // Collaborators: Add your names below using the format:
+        // System.out.println("Your Full Name");
+        // System.out.println("John peter elloren);
     }
 }
