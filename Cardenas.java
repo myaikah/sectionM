5118634741933080268b07b993ef1f04eb58e2cf
clarence3251 <cpcardenas@paterostechnologicalcollege.edu.ph>
2025-02-26 20:12:48 +0800

    Added Cardenas.java

---
+++ Cardenas.java
@@ -0,0 +1,5 @@
+public class Cardenas{
+  public static void display(){
+    System.out.println("Hello! This is Cruz contributing to the project");
+  }
+}
--- week4.java
+++ week4.java
@@ -3,6 +3,6 @@ public class week4 {
         System.out.println("Collaborative Java Project\n");
 
         student1.display(); // Already added
-        
+        Cardenas.display();
     }
 }
