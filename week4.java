39f6e6137a21c68746b1c29d6d7693f3f1466a6c
juntabiosGit <165021843+juntabiosGit@users.noreply.github.com>
2025-02-26 17:15:31 +0800

    Create week4.java for activity covering week 4

---
+++ week4.java
@@ -0,0 +1,8 @@
+public class week4 {
+    public static void main(String[] args) {
+        System.out.println("Collaborative Java Project\n");
+
+        Student1.display(); // Already added
+        Student2.display(); // New student adds their method here
+    }
+}
