87ecf39f89e813825d9497e2691cd28c1f5863a6
MARWIN MANGUNAY <mcmangunay@paterostechnologicalcollege.edu.ph>
2025-02-26 20:11:05 +0800

    Update week4.java

--- week4.java
+++ week4.java
@@ -2,7 +2,7 @@ public class week4 {
     public static void main(String[] args) {
         System.out.println("Collaborative Java Project\n");
 
-        student1.display(); // Already added
+        Mangunay.display(); // Already added
         
     }
 }
