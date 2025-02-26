d488d82972745c2b48913afe94303c9de9d4dd5f
juntabiosGit <165021843+juntabiosGit@users.noreply.github.com>
2025-02-26 19:54:53 +0800

    Update week4.java

--- week4.java
+++ week4.java
@@ -2,7 +2,7 @@ public class week4 {
     public static void main(String[] args) {
         System.out.println("Collaborative Java Project\n");
 
-        Student1.display(); // Already added
+        student1.display(); // Already added
         
     }
 }
