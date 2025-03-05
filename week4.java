74964875dbcb7407eefe7da13f67557f02a421ff
juntabiosGit <165021843+juntabiosGit@users.noreply.github.com>
2025-03-05 13:54:21 +0800

    FINAL COPY OF week4.java

--- week4.java
+++ week4.java
@@ -1,111 +1,28 @@
-
-
-public class week4 {
+public class Week4 {
 
     public static void main(String[] args) {
         System.out.println("Collaborative Java Project\n");
 
-        student1.display(); // Already added
-
+        // Call each student's method in alphabetical order
         Andes.display();
-
-        student1.display(); // Already added
-
-        DelaCruz.display();
-
-        Orolfo.dispaly(); // New student adds their method here
-
-        Cardenas.display();
-        Elloren.display();
-        Miranda.display();
-
-        Mangunay.display(); // Already added
         Batayola.display();
-
-        paciencia.display(); // Already added
-
-
-        bea.display(); // Already added
-
-
-
-
-        student1.display(); // Already added
-
-        DelaCruz.display();
-
         Beo.display();
-
-
-        Orolfo.dispaly(); // New student adds their method here
-
         Cardenas.display();
-        Elloren.display();
-
-        Mangunay.display(); // Already added
-
-        
-
-        paciencia.display(); // Already added
-        Intia.display();
-
-        Andes.display();
-        
-        student1.display(); // Already added
-
         DelaCruz.display();
-
-        Orolfo.dispaly(); // New student adds their method here
-
-        Cardenas.display();
-
         Elloren.display();
-
-        Mangunay.display(); // Already added
-
-        Batayola.display();
-
-        
-        paciencia.display();// Already added
-      
-        Student1.display(); // Already added
-        Fernandez.display();     // New student adds their method here
-
-
         Fernandez.display();
-
-
-
-
-
-        Gupid.display();
-
-
-        Laygan.display(); // Already added
-
-
-
-
-        Olimberio.display();
-
-        Gigante.display();
-
-
-
-
-        Gardoce.display(); //Already added
-
-        Mirandilla.display();
-
-
-
-
         Fonte.display();
-
-
-
-
+        Gardoce.display();
+        Gigante.display();
+        Gupid.display();
+        Intia.display();
+        Laygan.display();
+        Mangunay.display();
+        Miranda.display();
+        Mirandilla.display();
+        Olimberio.display();
+        Orolfo.display(); 
+        Paciencia.display();
+        Student1.display();
     }
 }
-
-
