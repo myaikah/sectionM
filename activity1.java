7a43c1116d953eb41c67d9113a5936fa6dafa280
imaikah <aaikahgabejan@gmail.com>
2025-04-04 11:22:59 +0800

    Added my name to Activity1.java

--- activity1.java
+++ activity1.java
@@ -1,53 +1,15 @@
 public class Activity1 {
     public static void main(String[] args) {
         System.out.println("List of Students:");
-    
+
         // Collaborators: Add your names below using the format:
         // System.out.println("Your Full Name");
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-        // Students, add your names below using the format:
-        // System.out.println("Your Name - SECTION");
-        // Example:
-        // System.out.println("Juan Dela Cruz - BSIT 2M");
-
-      System.out.println("Geremelyn Gigante")
-      System.out.println("Mykah Gabejan - BSIT 2M");
-      System.out.println("Roy Justine Ongue");
-      System.out.println("Lara Jean Gupid");
-      System.out.println("John peter elloren);
-      System.out.println("Mykah Gabejan - BSIT 2M");
-      System.out.println("Mykah Gabejan - BSIT 2M");
-      System.out.println("Clarence P. Cardenas - BSIT 2M");             
-      System.out.println("James Michael B. Dela Cruz - BSIT 2M");
-      System.out.println("Shaira S. Brisulda - BSIT 2M");
-      System.out.println("Xander Olimberio - BSIT 2M");
-      System.out.println("Stephanie Capuno - BSCS 2M");
-      System.out.println("Clarence P. Cardenas - BSIT 2M");
-      System.out.println("James Michael B. Dela Cruz - BSIT 2M");
-      System.out.println("Mykah Gabejan - BSIT 2M");
-      System.out.println("Ma. Liza M. Sugui - BSIT 2M"); 
-      System.out.println("Francis C. Andes - BSIT 2M");
-      System.out.println("Stephanie Capuno - BSCS 2M");
-      System.out.println("Clarence P. Cardenas - BSIT 2M");
-      System.out.println("James Michael B. Dela Cruz - BSIT 2M");
-      System.out.println("Stephen Laygan - BSIT 2M");
-      System.out.println ("Jayvon Paciencia - BSIT 2M"); 
-      System.out.println("Mykah Gabejan - BSIT 2M");
-    	System.out.println("Gerald Mirandilla - BSIT 2M");
-      System.out.println("John Carlo Gardoce - BSIT 2M");
-      System.out.println("James Micheal L. Batayola - BSIT 2M");
-    	System.out.println("Chelsie Kaye Bea - BSIT 2M");
-      System.out.println("Noah M. Lampa - BSIT 2M");
-      System.out.println("Kyle Neil Orolfo - BSIT 2M");
-      System.out.println("Stephen Rey Laygan BSIT 2M");
-
-=======
+        System.out.println("Geremelyn Gigante");
         System.out.println("Mykah Gabejan - BSIT 2M");
->>>>>>> 8098de6e8aebbece3849d3ba428f9f9444e3dba7
-=======
-        System.out.println("Mykah Gabejan - BSIT 2M");
->>>>>>> fcdc11df91544c8df85201f19d72b4e40cba3d27
+        System.out.println("Roy Justine Ongue");
+        System.out.println("Lara Jean Gupid");
+        System.out.println("John Peter Elloren");
     }
 }
+
