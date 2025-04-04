8098de6e8aebbece3849d3ba428f9f9444e3dba7
myaikah <aaikahgabejan@gmail.com>
2025-04-04 09:42:22 +0800

    Update activity1.java

--- activity1.java
+++ activity1.java
@@ -1,18 +1,10 @@
-public class StudentNames {
+public class Activity1 {
     public static void main(String[] args) {
         System.out.println("List of Students:");
+    
+        // Collaborators: Add your names below using the format:
+        // System.out.println("Your Full Name");
 
-<<<<<<< HEAD
-        // System.out.println("Mykah Gabejan - BSIT 2M");
-=======
-        // Students, add your names below using the format:
-        // System.out.println("Your Name - SECTION");
-        // Example:
-        // System.out.println("Juan Dela Cruz - BSIT 2M");
-	     
-      System.out.println("Clarence P. Cardenas - BSIT 2M");
-        System.out.println("James Michael B. Dela Cruz - BSIT 2M");
-      System.out.println("Mykah Gabejan - BSIT 2M");
-
+        System.out.println("Mykah Gabejan - BSIT 2M");
     }
 }
